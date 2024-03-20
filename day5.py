7f2612232970488a42eb24167bc80ab574c269d5
rajon111 <mostofarajon111@gmail.com>
2024-03-20 20:54:36 +0600

    task completed if statement basic

--- day5.py
+++ day5.py
@@ -11,6 +11,15 @@ if catsOrDogs == "cat":
 else:
  print("Woof")
 
+ '''
+ 1.Ask your users a series of questions that identify if they're one of the characters in the world you have created.
+
+ 2.Add multiple if statements to check the result of each question.
+
+ 3.Make sure to have a final print if they haven't selected any of the characters so far.
+ 
+ '''
+
 print('Marvel Movie Character Creator') 
 
 like = input("Do you like 'hanging around'?:")
