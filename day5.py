3e9cb94e48664154c8328a9ca95d723bc5d1c0fb
rajon111 <mostofarajon111@gmail.com>
2024-03-20 20:52:29 +0600

    if statement use

---
+++ day5.py
@@ -0,0 +1,20 @@
+myName = input("What's your name?: ")
+if myName == "Rajon":
+ print("Welcome Dude!")
+ print("You're just the baldest dude I've ever seen")
+else:
+ print("Who on earth are you?!")
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+ print("Woof")
+
+print('Marvel Movie Character Creator') 
+
+like = input("Do you like 'hanging around'?:")
+if like == "no":
+ print("Then you're not Spider-man")
+else:
+ print("My favorite") 
